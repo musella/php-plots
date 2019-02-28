410c245b8d021c5cd91df14d583bed584f59ab6a
Pasquale Musella <pasquale.musella@cern.ch>
2019-02-28 12:25:11 +0100

    zoom-in on single click

--- res/style.js
+++ res/style.js
@@ -1,23 +1,29 @@
 
-$(document).ready(function(){
-    $("img").dblclick(function() {
-	var animWidth, $this=$(this);
-	if( $this.hasClass('wide') ){
-            animWidth="40ex";
-	    if( $this.hasClass('thumb') ) {
-		/// this.src = this.src.replace(".","_thumb.");
-		this.src = this.src.substring( 0, this.src.lastIndexOf(".") ) + "_thumb" + this.src.substring( this.src.lastIndexOf(".") );
-	    }
-	}else{
-            animWidth="80ex";
-	    if( this.src.indexOf("_thumb" ) != -1){
-		$this.addClass('thumb');
-	    }
-	    this.src = this.src.replace("_thumb","");
+var myAnim = function() {
+    var animWidth, $this=$(this);
+    if( $this.parent().hasClass('ui-sortable-helper') ) { 
+	return; 
+    }
+    if( $this.hasClass('wide') ){
+	animWidth="40ex";
+	if( $this.hasClass('thumb') ) {
+	    /// this.src = this.src.replace(".","_thumb.");
+	    this.src = this.src.substring( 0, this.src.lastIndexOf(".") ) + "_thumb" + this.src.substring( this.src.lastIndexOf(".") );
 	}
-	$this.toggleClass('wide').animate({width: animWidth}, "fast");
+    }else{
+	animWidth="80ex";
+	if( this.src.indexOf("_thumb" ) != -1){
+	    $this.addClass('thumb');
+	}
+	this.src = this.src.replace("_thumb","");
+    }
+    $this.toggleClass('wide').animate({width: animWidth}, "fast");
+}
+
+$(document).ready(function(){
+	$("img").click(myAnim);
+	$("img").dblclick(myAnim);
+	$( "#piccont" ).sortable({
+		revert: true
+		    });
     });
-    $( "#piccont" ).sortable({
-	revert: true
-    });
-});
